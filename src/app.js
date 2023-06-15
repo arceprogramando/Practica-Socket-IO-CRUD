b0618c1060c763db829b5191d0a25b9bab37a4a5
Felipe Arce <arceprogramando@gmail.com>
2023-06-15 17:33:27 -0300

    primer commit  practica socket io con babel

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const app = express();
 // Settings
 app.set('port', 8000);
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 app.listen(app.get('port'), () => {
     console.log(`Server on post: \n http://localhost:${app.get('port')}`)
