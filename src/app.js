412d067eefa5df9599c1fe6ea67aec9672d0672b
Felipe Arce <arceprogramando@gmail.com>
2023-06-15 18:27:43 -0300

    agregado public

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
+import { __dirname } from './utils.js';
 
 // Initializations
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
+
 // Settings
 app.set('port', 8000);
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
 
 app.listen(app.get('port'), () => {
     console.log(`Server on post: \n http://localhost:${app.get('port')}`)
