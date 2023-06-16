4a00b6c8d1f89e0a7935901a9dc28bb033486089
Felipe Arce <arceprogramando@gmail.com>
2023-06-15 23:23:03 -0300

    actualizacion uuid

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import { __dirname } from './utils.js';
-import { Server } from 'socket.io'
-
+import { Server } from 'socket.io';
+import { v4 as uuid } from 'uuid';
 
 // Initializations
 const app = express();
-
+const notes = []
 // Settings
 app.set('port', 8000);
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 const server = app.listen(app.get('port'), () => {
-    console.log(`Server on post: \n http://localhost:${app.get('port')}`)
+    console.log(`Server on post: \n http://localhost:${app.get('port')}`);
 })
 
-const io = new Server(server)
+const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log('Nueva conexion', socket.id)
+    console.log('Nueva conexion', socket.id);
 
-    socket.on('client:newnote', data => {
-        console.log(data)
+    socket.on('client:newnote', newNote => {
+        const note = notes.push({ ...newNote, id: uuid() });
+        console.log(note);
     })
 
 })
